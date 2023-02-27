e4570e1540dcf2e273e48c5ea7f5aea46d10bcee
P-moloney <moloneypatrick@rocketmail.com>
2023-02-27 14:02:27 +0000

    Initial commit and add root route

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,6 @@
+class ApplicationController < ActionController::Base
+  # #hello method/action connects to routes
+  # def hello
+  #   render html: 'Hello world!'
+  # end
+end
---
+++ config/routes.rb
@@ -0,0 +1,4 @@
+Rails.application.routes.draw do
+  root 'pages#home'
+  #root 'application#hello' #tells rails to expect a hello method/action in application
+end
