042fe4ff5d6f4f1e8fbd21108db7ccfbaad22da4
P-moloney <moloneypatrick@rocketmail.com>
2023-02-27 15:14:25 +0000

    Add about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'pages#home'
+  get 'about', to: 'pages#about' # get request to about page
   #root 'application#hello' #tells rails to expect a hello method/action in application
 end
